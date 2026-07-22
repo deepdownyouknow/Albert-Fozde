7ad7e69472625cb9460d7f0e83cb651be9054a8b
deepdownyouknow <albert.szabolcs84@gmail.com>
2026-07-22 21:08:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fozde-v18';
+const CACHE = 'fozde-v19';
 const ASSETS = ['.', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png', 'apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
